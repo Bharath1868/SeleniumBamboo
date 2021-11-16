18439c9bf7b194ad26f6048b4163c84ae2c30eec
bharath <bharathkumark810@gmail.com>
2021-11-16 16:46:23 +0530

    commit

---
+++ src/test/java/TestSelenium/TestSelenium/SampleSelenium.java
@@ -0,0 +1,39 @@
+package TestSelenium.TestSelenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SampleSelenium {
+	public String baseUrl = "https://www.javatpoint.com/";  
+	String driverPath = "resources\\chromedriver.exe";  
+	public WebDriver driver ;   
+	@Test             
+	public void sample() {   
+		
+	System.setProperty("webdriver.chrome.driver", driverPath);  
+	
+	driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
+	driver.manage().window().maximize();  
+	driver.get(baseUrl);  
+	String URL= driver.getCurrentUrl();  
+	System.out.print(URL);  
+	String title = driver.getTitle();                  
+	System.out.println(title);  
+	}     
+	@BeforeTest  
+	public void beforeTest() {    
+	System.out.println("before test");  
+	}     
+	@AfterTest  
+	public void afterTest() {  
+	driver.quit();  
+	System.out.println("after test");  
+	}
+
+}
